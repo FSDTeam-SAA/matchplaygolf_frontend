b910d6d7a6c684c8be1708516dbbffea3db3b377
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-11-25 10:44:25 +0600

    reset password page done

---
+++ src/app/(auth)/reset-password/page.tsx
@@ -0,0 +1,18 @@
+import ResetPasswordForm from "./_components/reset-password-form";
+
+export default function ResetPasswordPage() {
+  return (
+    <div className="bg-[#ffffff7e] p-5 rounded-lg w-[400px] lg:w-[600px]">
+      <div className="text-center">
+        <h1 className="text-2xl font-medium">Reset Password</h1>
+        <p className="text-sm text-gray-700">
+          Enter new password to recover your password
+        </p>
+      </div>
+
+      <div className="mt-10">
+        <ResetPasswordForm />
+      </div>
+    </div>
+  );
+}
