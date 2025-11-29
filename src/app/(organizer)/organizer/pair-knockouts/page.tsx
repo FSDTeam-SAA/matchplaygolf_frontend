bdcb19c7c7a24f8f4030e325d24503aacccceee1
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-11-29 09:48:04 +0600

    organizer dashboard setup

---
+++ src/app/(organizer)/organizer/pair-knockouts/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PairKnockoutsPage = () => {
+  return (
+    <div>
+      Pair Knockouts Page 
+    </div>
+  )
+}
+
+export default PairKnockoutsPage
---
+++ src/app/(organizer)/organizer/tournaments-management/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TournamentsManagementPage = () => {
+  return (
+    <div>
+      Tournaments ManagementPage
+    </div>
+  )
+}
+
+export default TournamentsManagementPage
