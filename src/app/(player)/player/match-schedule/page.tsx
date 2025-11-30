42a0de080542036802072345bd38d7e44d6f67fe
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-11-30 11:16:50 +0600

    match schedule page design done

--- src/app/(player)/player/match-schedule/page.tsx
+++ src/app/(player)/player/match-schedule/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import AppTopBar from "../../_components/shared/app-topbar";
+import MatchSchedule from "./_components/match-schedule";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <AppTopBar
+        title="Match Schedule"
+        desc="View your upcoming matches and completed results"
+      />
 
-export default page
+      <div className="p-5 space-y-8">
+        <MatchSchedule />
+      </div>
+    </div>
+  );
+};
+
+export default page;
