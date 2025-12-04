268fde9d348dfdb21e181a2cd8ca619b99a31f56
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-12-04 10:55:23 +0600

    match management api integration

--- src/app/(organizer)/organizer/matches-management/_components/matches-management-container.tsx
+++ src/app/(organizer)/organizer/matches-management/_components/matches-management-container.tsx
@@ -13,99 +13,72 @@ import { Plus, Trash } from "lucide-react";
 import MatchPlayGolfPagination from "@/components/ui/matchplaygolf-pagination";
 import { Input } from "@/components/ui/input";
 import DeleteModal from "@/components/modals/delete-modal";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import TableSkeletonWrapper from "@/components/shared/TableSkeletonWrapper/TableSkeletonWrapper";
+import ErrorContainer from "@/components/shared/ErrorContainer/ErrorContainer";
+import NotFound from "@/components/shared/NotFound/NotFound";
+import { useSession } from "next-auth/react";
+import { MatchApiResponse } from "./matches-data-type";
+import moment from "moment";
 
 const MatchesManagementContainer = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [search, setSearch] = useState("");
   const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [matchId, setMatchId] = useState("");
+
+  const queryClient = useQueryClient();
+  const session = useSession();
+  const token = (session?.data?.user as { accessToken: string })?.accessToken;
+  console.log(token);
+
   console.log(search);
-  const mockMatches = [
-    {
-      "Match ID": "M001",
-      "Tournament Name": "Spring Championship",
-      Round: "Final",
-      "Player 1": "Michael Johnson",
-      "Player 2": "David Thompson",
-      Score: "3-2",
-      Date: "May 15, 2023",
-      Status: "Completed",
-    },
-    {
-      "Match ID": "M002",
-      "Tournament Name": "Summer Invitational",
-      Round: "Quarter Final",
-      "Player 1": "Sarah Williams",
-      "Player 2": "Emma Davis",
-      Score: "1-1",
-      Date: "Jun 12, 2023",
-      Status: "Ongoing",
-    },
-    {
-      "Match ID": "M003",
-      "Tournament Name": "Pro-Arm Tournament",
-      Round: "Round 1",
-      "Player 1": "James Wilson",
-      "Player 2": "Michael Johnson",
-      Score: "-",
-      Date: "Jun 22, 2023",
-      Status: "Upcoming",
-    },
-    {
-      "Match ID": "M004",
-      "Tournament Name": "Regional Qualifier",
-      Round: "Semi Final",
-      "Player 1": "David Thompson",
-      "Player 2": "Sarah Williams",
-      Score: "-",
-      Date: "Jul 8, 2023",
-      Status: "Upcoming",
-    },
-    {
-      "Match ID": "M005",
-      "Tournament Name": "Championship 2025",
-      Round: "Final",
-      "Player 1": "Michael Johnson",
-      "Player 2": "David Thompson",
-      Score: "7-5",
-      Date: "Jan 06, 2025",
-      Status: "Completed",
-    },
-    {
-      "Match ID": "M006",
-      "Tournament Name": "Annual Ship 2025",
-      Round: "Round 2",
-      "Player 1": "James Wilson",
-      "Player 2": "James Wilson",
-      Score: "0-1",
-      Date: "Jan 06, 2025",
-      Status: "Ongoing",
-    },
-  ];
 
-  const handleDelete = () => {};
-  return (
-    <div>
-      {/* table container */}
-      <div className="p-6 space-y-6">
-        {/* table header  */}
-        <div className="w-full flex items-center justify-between">
-          <div>
-            <Input
-              type="search"
-              className="w-[479px] h-[48px] border border-[#C0C3C1] rounded-[4px] outline-none right-0 text-base font-medium leading-[120%] text-[#343A40]"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-              placeholder="Search..."
-            />
-          </div>
-          <div>
-            <button className="flex items-center gap-2 bg-[#DF1020] py-3 px-9 rounded-[8px] text-[#F8F9FA] text-base font-medium leading-[150%] ">
-              <Plus /> Create Match
-            </button>
-          </div>
-        </div>
+  // get tournament api
+  const { data, isLoading, isError, error } = useQuery<MatchApiResponse>({
+    queryKey: ["matches", currentPage],
+    queryFn: async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/match?page=${currentPage}&limit=10`
+      );
+      return res.json();
+    },
+  });
 
-        {/* table  */}
+  let content;
+
+  if (isLoading) {
+    content = (
+      <div>
+        <TableSkeletonWrapper count={5} />
+      </div>
+    );
+  } else if (isError) {
+    content = (
+      <div>
+        <ErrorContainer message={error?.message || "Something went wrong"} />
+      </div>
+    );
+  } else if (
+    data &&
+    data?.data &&
+    data?.data?.matches &&
+    data?.data?.matches?.length === 0
+  ) {
+    content = (
+      <div>
+        <NotFound message="Oops! No data available. Modify your filters or check your internet connection." />
+      </div>
+    );
+  } else if (
+    data &&
+    data?.data &&
+    data?.data?.matches &&
+    data?.data?.matches?.length > 0
+  ) {
+    content = (
+      <div>
         <Table className="">
           <TableHeader className="bg-[#FCE7E9] rounded-t-[12px]">
             <TableRow className="">
@@ -139,9 +112,9 @@ const MatchesManagementContainer = () => {
             </TableRow>
           </TableHeader>
           <TableBody className="border-b border-x border-[#E6E7E6] rounded-b-[12px]">
-            {mockMatches?.map((item) => {
+            {data?.data?.matches?.map((item) => {
               return (
-                <TableRow key={item?.["Match ID"]} className="">
+                <TableRow key={item?._id} className="">
                   <TableCell className="text-base font-medium text-[#68706A] leading-[150%] pl-6 py-4">
                     {item?.["Match ID"]}
                   </TableCell>
@@ -149,38 +122,39 @@ const MatchesManagementContainer = () => {
                     {item?.["Tournament Name"]}
                   </TableCell>
                   <TableCell className="text-base font-normal text-[#68706A] leading-[150%] text-center py-4">
-                    {item?.Round}
+                    {item?.roundId?.roundName}
                   </TableCell>
                   <TableCell className="text-base font-normal text-[#68706A] leading-[150%] text-center py-4">
-                    {item?.["Player 1"]}
+                    {item?.player1Id?.fullName}
                   </TableCell>
                   <TableCell className="text-base font-medium text-[#343A40] leading-[150%] text-center py-4">
-                    {item?.["Player 2"]}
+                    {item?.player2Id?.fullName}
                   </TableCell>
                   <TableCell className="text-base font-normal text-[#68706A] leading-[150%] text-center py-4">
-                    {item?.Score}
+                    {item?.score}
                   </TableCell>
                   <TableCell className="text-base font-medium text-[#343A40] leading-[150%] text-center py-4">
-                    {item?.Date}
+                    {moment(item?.createdAt).format("MMM DD YYYY")}
                   </TableCell>
 
                   <TableCell className="text-base font-medium text-[#68706A] leading-[150%] text-center py-4">
                     <button
                       className={`w-[140px] h-[40px] ${
-                        item?.Status === "Ongoing"
+                        item?.status === "Ongoing"
                           ? "bg-[#E6FAEE] text-[#27BE69] py-2 px-4"
-                          : item?.Status === "Upcoming"
+                          : item?.status === "Upcoming"
                           ? "bg-[#EFF6FF] text-[#2563EB] py-2 px-4"
                           : "bg-[#E8E8E8] text-[#6C757D] py-2 px-4"
                       }`}
                     >
-                      {item?.Status}
+                      {item?.status}
                     </button>
                   </TableCell>
                   <TableCell className="flex items-center justify-center gap-6 py-4">
                     <button
                       onClick={() => {
                         setDeleteModalOpen(true);
+                        setMatchId(item?._id);
                       }}
                       className="cursor-pointer"
                     >
@@ -192,20 +166,89 @@ const MatchesManagementContainer = () => {
             })}
           </TableBody>
         </Table>
+      </div>
+    );
+  }
 
-        {/* pagination  */}
-        <div className="w-full flex items-center justify-between py-6">
-          <p className="text-base font-normal text-[#68706A] leading-[150%]">
-            Showing 1 to 5 of 12 results
-          </p>
+  console.log(data);
+
+  // delete match api
+  const { mutate } = useMutation({
+    mutationKey: ["delete-match"],
+    mutationFn: async (id: string) => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/match/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return res.json();
+    },
+    onSuccess: (data) => {
+      if (!data?.success) {
+        toast.error(data?.message || "Something went wrong");
+        return;
+      }
+      toast.success(data?.message || "Matches deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["matches"] });
+    },
+  });
+
+  const handleDelete = () => {
+    if (matchId) {
+      mutate(matchId);
+    }
+    setDeleteModalOpen(false);
+  };
+
+  return (
+    <div>
+      {/* table container */}
+      <div className="p-6 space-y-6">
+        {/* table header  */}
+        <div className="w-full flex items-center justify-between">
           <div>
-            <MatchPlayGolfPagination
-              currentPage={currentPage}
-              totalPages={10}
-              onPageChange={(page) => setCurrentPage(page)}
+            <Input
+              type="search"
+              className="w-[479px] h-[48px] border border-[#C0C3C1] rounded-[4px] outline-none right-0 text-base font-medium leading-[120%] text-[#343A40]"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search..."
             />
           </div>
+          <div>
+            <button className="flex items-center gap-2 bg-[#DF1020] py-3 px-9 rounded-[8px] text-[#F8F9FA] text-base font-medium leading-[150%] ">
+              <Plus /> Create Match
+            </button>
+          </div>
         </div>
+
+        {/* table  */}
+        <div>{content}</div>
+
+        {/* pagination  */}
+        {data &&
+          data?.data &&
+          data?.data?.pagination &&
+          data?.data?.pagination?.totalPages > 1 && (
+            <div className="w-full flex items-center justify-between py-6">
+              <p className="text-base font-normal text-[#68706A] leading-[150%]">
+                Showing {currentPage} to 10 of {data?.data?.pagination?.total}{" "}
+                results
+              </p>
+              <div>
+                <MatchPlayGolfPagination
+                  currentPage={currentPage}
+                  totalPages={data?.data?.pagination?.total}
+                  onPageChange={(page) => setCurrentPage(page)}
+                />
+              </div>
+            </div>
+          )}
 
         {/* delete modal  */}
         {deleteModalOpen && (
