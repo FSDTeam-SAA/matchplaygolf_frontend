1bd0b5102e924e7267da1b4f0b32742e02331f37
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-12-20 11:49:28 +0600

    personal information form problem fix up

--- src/app/(organizer)/organizer/settings/personal-information/_components/personal-information-data-type.tsx
+++ src/app/(organizer)/organizer/settings/personal-information/_components/personal-information-data-type.tsx
@@ -1,29 +1,61 @@
+// export interface ProfileApiResponse {
+//   success: boolean;
+//   message: string;
+//   data: ProfileData;
+// }
+
+// export interface ProfileData {
+//   organizerLogo: string;
+//   _id: string;
+//   fullName: string;
+//   email: string;
+//   phone: string;
+//   organizationName: string;
+//   dob: string; // ISO date string
+//   gender: "male" | "female" | "other" | string;
+//   role: string;
+//   clubName: string;
+//   handicap: string;
+//   whsNumber: string;
+//   profileImage: string;
+//   isVerified: boolean;
+//   createdAt: string;
+//   updatedAt: string;
+//   color: string;
+//   country: string;
+//   newsletterPreference: "subscribe" | "unsubscribe" | string;
+//   receiveOrderUpdates: boolean;
+//   sportNationalId: string;
+// }
+
 export interface ProfileApiResponse {
-  success: boolean;
-  message: string;
-  data: ProfileData;
+  success: boolean
+  message: string
+  data: ProfileData
 }
 
+
 export interface ProfileData {
-  organizerLogo: string;
-  _id: string;
-  fullName: string;
-  email: string;
-  phone: string;
-  organizationName: string;
-  dob: string; // ISO date string
-  gender: "male" | "female" | "other" | string;
-  role: string;
-  clubName: string;
-  handicap: string;
-  whsNumber: string;
-  profileImage: string;
-  isVerified: boolean;
-  createdAt: string;
-  updatedAt: string;
-  color: string;
-  country: string;
-  newsletterPreference: "subscribe" | "unsubscribe" | string;
-  receiveOrderUpdates: boolean;
-  sportNationalId: string;
+  _id: string
+  fullName: string
+  email: string
+  phone: string
+  organizationName: string
+  dob: string // ISO date string
+  gender: "Male" | "Female" | "Other"
+  role: "Organizer" | "User" | "Admin"
+  clubName: string
+  country: string
+  handicap: string
+  whsNumber: string
+  color: string
+  newsletterPreference: "subscribe" | "unsubscribe"
+  receiveOrderUpdates: boolean
+  profileImage: string
+  organizerLogo: string
+  sportNationalId: string
+  isVerified: boolean
+  createdAt: string // ISO date string
+  updatedAt: string // ISO date string
 }
+
