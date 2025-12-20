a8101593d12549308ad0ff4c6668522755296ce8
Kongkon-79 <kongkonjowarder79@gmail.com>
2025-12-20 10:55:12 +0600

    players management route add

---
+++ src/app/(organizer)/organizer/players-management/_components/players-management-container.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PlayersManagementContainer = () => {
+  return (
+    <div>PlayersManagementContainer</div>
+  )
+}
+
+export default PlayersManagementContainer
---
+++ src/app/(organizer)/organizer/players-management/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PlayersManagementContainer from './_components/players-management-container'
+import PlayersManagementHeader from './_components/players-management-header'
+
+const PalyersManagementPage = () => {
+  return (
+    <div>
+        <PlayersManagementHeader />
+        <PlayersManagementContainer/>
+    </div>
+  )
+}
+
+export default PalyersManagementPage
