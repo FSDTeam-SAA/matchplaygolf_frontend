b1861b9b1b5e8973e9c9a7f50e537c1e25f30d54
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-12-24 15:20:54 +0600

    match moments done

--- src/app/(website)/tournaments/[id]/_components/draw.tsx
+++ src/app/(website)/tournaments/[id]/_components/draw.tsx
@@ -49,7 +49,7 @@ export interface Match {
   pair1Id: PairId;
   pair2Id: PairId;
   comments: string;
-  matchPhoto: string[]
+  matchPhoto: string[];
 }
 
 interface Props {
@@ -61,10 +61,12 @@ const Draw = ({ matches, isLoading }: Props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isVsModalOpen, setIsVsModalOpen] = useState(false);
   const [matchInfo, setMatchInfo] = useState<Match>();
+  const [winner1, setWinner1] = useState<boolean>();
 
-  const handleOpenModal = (match: Match) => {
+  const handleOpenModal = (match: Match, winner1: boolean) => {
     setIsModalOpen(true);
     setMatchInfo(match);
+    setWinner1(winner1);
   };
 
   const handleVsOpen = (match: Match) => {
@@ -292,7 +294,7 @@ const Draw = ({ matches, isLoading }: Props) => {
                       {item.status === "completed" && (
                         <div>
                           <button
-                            onClick={() => handleOpenModal(item)}
+                            onClick={() => handleOpenModal(item, winner1)}
                             className="text-primary font-semibold text-sm"
                           >
                             Moments
@@ -320,6 +322,7 @@ const Draw = ({ matches, isLoading }: Props) => {
           isModalOpen={isModalOpen}
           handleCloseModal={handleCloseModal}
           match={matchInfo as Match}
+          winner1={winner1 as boolean}
         />
       )}
 
--- src/app/(website)/tournaments/[id]/_components/moments-modal.tsx
+++ src/app/(website)/tournaments/[id]/_components/moments-modal.tsx
@@ -9,6 +9,7 @@ interface Props {
   handleCloseModal: () => void;
   match: Match;
   pairWinner1?: boolean;
+  winner1?: boolean;
 }
 
 const MomentsModal = ({
@@ -16,6 +17,7 @@ const MomentsModal = ({
   handleCloseModal,
   match,
   pairWinner1,
+  winner1,
 }: Props) => {
   return (
     <Dialog open={isModalOpen} onOpenChange={handleCloseModal}>
@@ -50,12 +52,27 @@ const MomentsModal = ({
                 </h1>
               </div>
             ) : (
-              <div></div>
+              <div className="flex items-center gap-4">
+                <h1>{match?.player1Id?.fullName}</h1>
+
+                <div
+                  className={`flex items-center gap-2 ${
+                    winner1 && "flex-row-reverse"
+                  }`}
+                >
+                  <h1>VS</h1>{" "}
+                  <h1>
+                    {match?.player1Score}/{match?.player2Score}
+                  </h1>
+                </div>
+
+                <h1>{match?.player2Id?.fullName}</h1>
+              </div>
             )}
           </div>
 
           <div className="mb-5">
-            <p className="opacity-75">{match?.comments}</p>
+            <p className="opacity-75">{match?.comments || "No comments."}</p>
           </div>
 
           <div className="grid grid-cols-3 gap-5">
