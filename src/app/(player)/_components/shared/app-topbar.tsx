beb702ecfaea315f1e99870b15808afb810e7b9d
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-12-25 10:40:05 +0600

    user dashboard modify

--- src/app/(player)/_components/shared/app-topbar.tsx
+++ src/app/(player)/_components/shared/app-topbar.tsx
@@ -1,8 +1,12 @@
-import { Bell } from "lucide-react";
-import Link from "next/link";
+"use client";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import React from "react";
 
 const AppTopBar = ({ title, desc }: { title: string; desc: string }) => {
+  const session = useSession();
+  const profileImage = session?.data?.user?.profileImage;
+
   return (
     <div className="bg-white py-4 px-5 flex items-center justify-between sticky top-0 z-50">
       <div>
@@ -10,12 +14,14 @@ const AppTopBar = ({ title, desc }: { title: string; desc: string }) => {
         <p className="text-gray-600 text-sm mt-1">{desc}</p>
       </div>
 
-      <div>
-        <Link href={"/player/notification"}>
-          <button>
-            <Bell />
-          </button>
-        </Link>
+      <div className="h-12 w-12 rounded-full">
+        <Image
+          src={profileImage || "/images/common/user_placeholder.png"}
+          alt="img.png"
+          width={1000}
+          height={1000}
+          className="h-full w-full rounded-full object-cover"
+        />
       </div>
     </div>
   );
